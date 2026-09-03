eafb8e75e0e28de123379a8a7ee2d6e08a27dc1d
Ngomelaezekia <ezekiangomela@gmail.com>
2026-09-03 09:51:11 +0300

    Improve post feed index

--- backend/models/post.go
+++ backend/models/post.go
@@ -4,11 +4,11 @@ import "time"
 
 type Post struct {
 	ID          uint      `gorm:"primaryKey" json:"postId"`
-	UserID      uint      `gorm:"not null;index:idx_posts_user_created" json:"userId"`
+	UserID      uint      `gorm:"not null;index:idx_posts_user_created,priority:1" json:"userId"`
 	ContentType string    `gorm:"not null" json:"contentType"`
 	ContentURL  string    `gorm:"not null" json:"contentUrl"`
 	Caption     string    `gorm:"type:text" json:"caption,omitempty"`
-	CreatedAt   time.Time `json:"createdAt"`
+	CreatedAt   time.Time `gorm:"index:idx_posts_user_created,priority:2" json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
 
 	LikeCount int  `gorm:"-" json:"likeCount"`
