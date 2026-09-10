ea994d3a6f2e812445bfce0b6cc3a338038af9c9
Ngomelaezekia <ezekiangomela@gmail.com>
2026-09-10 22:13:33 +0300

    Deduplicate media observability event definitions

--- backend/observability/events.go
+++ backend/observability/events.go
@@ -1,23 +1,5 @@
 package observability
 
-import "log"
-
-func MediaJobClaimed(jobID uint) {
-	log.Printf("event=MEDIA_JOB_CLAIMED job_id=%d", jobID)
-}
-
-func MediaProcessingStarted(jobID, uploadID uint) {
-	log.Printf("event=MEDIA_PROCESSING_STARTED job_id=%d upload_id=%d", jobID, uploadID)
-}
-
-func MediaProcessingCompleted(jobID uint) {
-	log.Printf("event=MEDIA_PROCESSING_COMPLETED job_id=%d", jobID)
-}
-
-func MediaProcessingFailed(jobID uint, err error) {
-	log.Printf("event=MEDIA_PROCESSING_FAILED job_id=%d error=%v", jobID, err)
-}
-
 func MediaStorageFailed(operation string, err error) {
-	log.Printf("event=MEDIA_STORAGE_FAILED operation=%s error=%v", operation, err)
+	logEvent("MEDIA_STORAGE_FAILED", map[string]any{"operation": operation, "error": err.Error()})
 }
