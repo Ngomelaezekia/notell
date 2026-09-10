da6e395a1708b71c74397fcc404901272d7ad58d
Ngomelaezekia <ezekiangomela@gmail.com>
2026-09-10 22:13:52 +0300

    Align media observability events with worker contract

--- backend/observability/media_events.go
+++ backend/observability/media_events.go
@@ -4,12 +4,12 @@ func MediaJobClaimed(jobID uint) {
 	Event("MEDIA_JOB_CLAIMED", map[string]any{"job_id": jobID})
 }
 
-func MediaProcessingStarted(jobID uint) {
-	Event("MEDIA_PROCESSING_STARTED", map[string]any{"job_id": jobID})
+func MediaProcessingStarted(jobID, uploadID uint) {
+	Event("MEDIA_PROCESSING_STARTED", map[string]any{"job_id": jobID, "upload_id": uploadID})
 }
 
-func MediaProcessingCompleted(jobID uint, seconds float64) {
-	Event("MEDIA_PROCESSING_COMPLETED", map[string]any{"job_id": jobID, "duration": seconds})
+func MediaProcessingCompleted(jobID uint) {
+	Event("MEDIA_PROCESSING_COMPLETED", map[string]any{"job_id": jobID})
 }
 
 func MediaProcessingFailed(jobID uint, err error) {
