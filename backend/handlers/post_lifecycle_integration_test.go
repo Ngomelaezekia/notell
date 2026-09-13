5dde36c386b00981a5f1a70d8ae987191122418c
Ngomelaezekia <ezekiangomela@gmail.com>
2026-09-13 14:12:54 +0300

    Fix media metadata test cleanup

--- backend/handlers/post_lifecycle_integration_test.go
+++ backend/handlers/post_lifecycle_integration_test.go
@@ -86,11 +86,18 @@ func createLifecycleUser(t *testing.T, db *gorm.DB, suffix string) models.User {
 		if len(postIDs) > 0 {
 			_ = db.Where("post_id IN ?", postIDs).Delete(&models.PostMusic{}).Error
 			_ = db.Where("post_id IN ?", postIDs).Delete(&models.Notification{}).Error
-			_ = db.Where("post_id IN ?", postIDs).Delete(&models.Upload{}).Error
-			_ = db.Where("id IN ?", postIDs).Delete(&models.Post{}).Error
 		}
-		_ = db.Where("user_id = ?", user.ID).Delete(&models.MediaMetadata{}).Error
+
+		var uploadIDs []uint
+		_ = db.Model(&models.Upload{}).Where("user_id = ?", user.ID).Pluck("id", &uploadIDs).Error
+		if len(uploadIDs) > 0 {
+			_ = db.Where("upload_id IN ?", uploadIDs).Delete(&models.MediaMetadata{}).Error
+		}
+		if len(postIDs) > 0 {
+			_ = db.Where("post_id IN ?", postIDs).Delete(&models.Upload{}).Error
+		}
 		_ = db.Where("user_id = ?", user.ID).Delete(&models.Upload{}).Error
+		_ = db.Where("id IN ?", postIDs).Delete(&models.Post{}).Error
 		_ = db.Where("actor_id = ? OR user_id = ?", user.ID, user.ID).Delete(&models.Notification{}).Error
 		_ = db.Delete(&user).Error
 	})
