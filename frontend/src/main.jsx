f7a5bf5a532e35f1f20e6d888ad125c5e3911e31
Ngomelaezekia <ezekiangomela@gmail.com>
2026-09-14 09:40:43 +0300

    Enable post action and caption refinement styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./features/postComposer/mediaStudioPolish.css";
+import "./post-card-refinement.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-        <App />
+    <App />
   </React.StrictMode>
 );
